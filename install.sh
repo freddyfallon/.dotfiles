dcde20f36945c2f7e41387ad5b117417d4fe8b41
Freddy Fallon <freddyfallon@gmail.com>
2019-07-05 16:49:20 +0100

    🔧 Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,70 @@
 #!/bin/bash
 homebrew=/usr/local/Homebrew
 rcm=/usr/local/Cellar/rcm
+shell=$( echo $SHELL)
+echo "---------------------------------------------------------"
 if [ ! -d "$homebrew" ]; then
   echo "Homebrew not installed, installing"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  echo "Installing rcm"
-  brew tap thoughtbot/formulae
-  brew install rcm
-  echo "Setting dotfiles"
-  env RCRC=$HOME/dotfiles/rcrc rcup
-  echo "Done!"
-elif [ ! -d "$rcm" ]; then
-  echo "Homebrew already installed, installing rcm"
-  brew tap thoughtbot/formulae
-  brew install rcm
   echo "Setting dotfiles"
   env RCRC=$HOME/dotfiles/rcrc rcup
   echo "Done!"
 else
-  echo "Homebrew and rcm already installed"
-  echo "Setting dotfiles"
-  env RCRC=$HOME/dotfiles/rcrc rcup
-  echo "Done!"
+  echo "Homebrew installed"
 fi
+
+echo "---------------------------------------------------------"
+
+if [ ! -d "$rcm" ]; then
+  echo "rcm not installed, installing"
+  brew tap thoughtbot/formulae
+  brew install rcm
+  echo "Done!"
+else
+  echo "rcm installed"
+fi
+
+packages=(
+"git"
+"node"
+"tmux"
+"neovim"
+"zsh"
+)
+
+echo "---------------------------------------------------------"
+
+localGit="/usr/local/bin/git"
+if [ -f "$localGit" ]
+then
+  echo "git installed"
+else
+  echo "git is not installed"
+fi
+
+echo "---------------------------------------------------------"
+
+if [ "$shell" != "/bin/zsh" ]; then
+echo shell
+
+echo "---------------------------------------------------------"
+
+echo "Changing to zsh"
+chsh -s $(which zsh)
+
+echo "You'll need to log out for this to take effect"
+
+else
+  echo "zsh installed"
+fi
+
+echo "---------------------------------------------------------"
+
+cd $HOME
+echo "Running rcup command"
+echo "This is symlink the rc files in .dofiles"
+echo "with the rc files in $HOME"
+
+rcup
+
+echo "---------------------------------------------------------"
