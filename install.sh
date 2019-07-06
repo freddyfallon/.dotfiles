ee31e8579f9189f98341830be9fdcdbfcb795164
Freddy Fallon <freddyfallon@gmail.com>
2019-07-06 11:57:40 +0100

    ♻️ Cleaning up install.sh a bit

--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 echo "INSTALLING"
+
 homebrew=/usr/local/Homebrew
 rcm=/usr/local/Cellar/rcm
 shell=$( echo $SHELL)
+localRepo="$HOME/dotfiles"
+localGit="/usr/local/bin/git"
+ohMyZsh="$HOME/.oh-my-zsh"
+syntaxHighlighting="$HOME/.oh-my-zsh/zsh-syntax-highlighting"
+
 echo "---------------------------------------------------------"
+
 if [ ! -d "$homebrew" ]; then
   echo "Homebrew not installed, installing"
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -26,9 +33,10 @@ fi
 packages=(
 "git"
 "tmux"
+"nvm"
 "vim"
 "zsh"
-"nvm"
+"z"
 )
 
 for i in "${packages[@]}"
@@ -44,7 +52,31 @@ done
 
 echo "---------------------------------------------------------"
 
-localGit="/usr/local/bin/git"
+caskPackages=(
+  "iterm2"
+  "authy"
+  "caffeine"
+  "docker"
+  "karabiner-elements"
+  "postman"
+  "rocket"
+  "sizeup"
+  "visual-studio-code"
+  "firefox"
+  "google-chrome"
+)
+
+for i in "${caskPackages[@]}"
+do
+  installed=$(brew cask list | grep -w $i)
+  if [ ! "$installed" ]; then
+    echo "$i not installed, installing"
+    brew cask install $i
+    echo "$i installed"
+    echo "---------------------------------------------------------"
+  fi
+done
+
 if [ -f "$localGit" ]
 then
   echo "git installed"
@@ -54,23 +86,22 @@ fi
 
 echo "---------------------------------------------------------"
 
-localRepo=$(ls $HOME | grep -w dotfiles)
-
-if [ ! "$localRepo" ]; then
+if [ -d "$localRepo" ]; then
+  echo "dotfiles repo found at ~/"
+  else 
   echo "Cloning dotfiles"
   git clone https://github.com/freddyfallon/dotfiles.git ~/dotfiles
+  echo "dotfiles now cloned to ~/"
 fi
 
 echo "---------------------------------------------------------"
 
 if [ "$shell" != "/bin/zsh" ]; then
-echo shell
 
 echo "---------------------------------------------------------"
 
 echo "Changing to zsh"
 chsh -s $(which zsh)
-
 echo "You'll need to log out for this to take effect"
 
 else
@@ -79,9 +110,29 @@ fi
 
 echo "---------------------------------------------------------"
 
+if [ -d "$ohMyZsh" ]; then
+  echo "oh-my-zsh installed"
+else
+  echo "oh-my-zsh not installed, installing"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  echo "oh-my-zsh installed"
+fi
+
+echo "---------------------------------------------------------"
+
+if [ -d "$syntaxHighlighting" ]; then
+  echo "zsh syntax highlighting installed"
+else
+  echo "zsh syntax highlighting not installed, installing"
+  cd $HOME/.oh-my-zsh && git clone git://github.com/zsh-users/zsh-syntax-highlighting.git && cd -
+  echo "zsh syntax highlighting installed"
+fi
+
+echo "---------------------------------------------------------"
+
 cd $HOME
 echo "Running rcup command"
-echo "This is symlink the rc files in .dofiles"
+echo "This symlinks the rc files in dotfiles"
 echo "with the rc files in $HOME"
 
 env RCRC=$HOME/dotfiles/rcrc rcup
